e65c38fdd65dbdc79c67e9aadc336ec3aaeda3f4
Lucas Bonomi <lucas.bonomi@gmail.com>
2019-06-10 23:57:02 +0200

    Update

---
+++ build/precache-manifest.2c5abc16160c072f37bd73941e3c1165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdfc7519959ae6da097e291999a3d5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb45fae6472f16773e1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2294d832adb064040746",
+    "url": "/static/js/2.7a2f0005.chunk.js"
+  },
+  {
+    "revision": "deb45fae6472f16773e1",
+    "url": "/static/js/main.f981d345.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import { css, cx } from "emotion";
+// import Client from "fortnite";
 import "./App.css";
+
+// const fortnite = new Client("425c6cf9-c137-4149-977f-377b7bd33f06");
+
+const team = [
+  { name: "iamfabriceg", uid: "2b7897075f20421b81529bdc2ab742f3" },
+  { name: "SKIDIP lukyvj", uid: "ff29ca89e591485e8d450c2aec558c15" },
+  { name: "SKIPID AntShelby", uid: "c605a87eba634e8dbfc7fba4448fa87d" },
+  { name: "SKIDIP Kingom", uid: "0d4f175a990f44a08e0447b56ed42703" }
+];
 
 const Board = props => (
   <div
@@ -9,40 +19,105 @@ const Board = props => (
     `}
   >
     <h1>@{props.name}</h1>
-    <iframe
-      src={`https://fortnitetracker.com/profile/psn/${props.name}`}
-      frameborder="0"
-      title={props.name}
-      className={css`
-        width: 100%;
-        height: 100%;
-      `}
-    />
+    <div>
+      <p>
+        wins: <b>{props.wins}</b>
+      </p>
+      <p>
+        kills: <b>{props.kills}</b>
+      </p>
+      <p>
+        kd: <b>{props.kd}</b>
+      </p>
+      <p>
+        matchesplayed: <b>{props.matchesplayed}</b>
+      </p>
+      <p>
+        winrate: <b>{props.winrate}</b>
+      </p>
+    </div>
   </div>
 );
-function App() {
-  return (
-    <div
-      className={cx(
-        "App",
-        css`
-          display: grid;
-          grid-template-columns: repeat(4, 1fr);
-          grid-gap: 10px;
-          grid-auto-rows: minmax(100px, auto);
-        `
-      )}
-    >
-      {[
-        "iamfabriceg",
-        "SKIDIP LUKYVJ",
-        "SKIPID AntShelby",
-        "SKIPID Kingom"
-      ].map(item => (
-        <Board name={item} />
-      ))}
-    </div>
-  );
+
+class LeaderBoard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    let statApi =
+      "https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats?user_id=" +
+      this.props.uid +
+      "&platform=ps4";
+    fetch(statApi)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data: [data] });
+      });
+  }
+
+  render() {
+    return this.state.data.map(hit => (
+      <Board
+        key={hit.uid}
+        name={hit.username}
+        wins={hit.totals.wins}
+        kills={hit.totals.kills}
+        kd={hit.totals.kd}
+        matchesplayed={hit.totals.matchesplayed}
+        winrate={hit.totals.winrate}
+      />
+    ));
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    let statApi =
+      "https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats?user_id=" +
+      team[0].uid +
+      "&platform=ps4";
+    let box = [];
+    fetch(statApi)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data: [data] });
+      });
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className={cx("App")}>
+        <h1>SKIDIP</h1>
+        <div
+          className={cx(
+            css`
+              display: grid;
+              grid-template-columns: repeat(4, 1fr);
+              grid-gap: 10px;
+              grid-auto-rows: minmax(100px, auto);
+            `
+          )}
+        >
+          {team.map(member => {
+            return <LeaderBoard uid={member.uid} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
