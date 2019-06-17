c88ab44df2fe948f4bde94a9907ba4c8d0ff9a08
Lucas Bonomi <lucas.bonomi@gmail.com>
2019-06-17 18:22:38 +0200

    update

--- precache-manifest.1e24e95cb48ccde462b1d49929f1af80.js
+++ precache-manifest.1e24e95cb48ccde462b1d49929f1af80.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4df39ec2fbf71f3417ab6c4e6d2f1d40",
+    "revision": "14183e9c543693ac2838bc72e49f5888",
     "url": "/index.html"
   },
   {
-    "revision": "af2f3585dc83b1050cbe",
+    "revision": "e4673f9175ac169e1686",
     "url": "/static/css/main.151da5ae.chunk.css"
   },
   {
-    "revision": "85c121e165cea2b4992d",
-    "url": "/static/js/2.8c2a5c8a.chunk.js"
+    "revision": "a89f9c637580bde7eef0",
+    "url": "/static/js/2.d1c8fe9a.chunk.js"
   },
   {
-    "revision": "af2f3585dc83b1050cbe",
-    "url": "/static/js/main.f1715843.chunk.js"
+    "revision": "e4673f9175ac169e1686",
+    "url": "/static/js/main.7f97bc72.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
